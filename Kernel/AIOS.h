106fc17e77edb801b3316ec6785b829386f9758f
SenseRate <zhangxb@aliyun.com>
2017-03-08 22:49:48 +0800

    Add files via upload

--- Kernel/AIOS.h
+++ Kernel/AIOS.h
@@ -53,9 +53,9 @@ extern "C" {
 #include "OSMutex.h"
 #include "OSTimer.h"
 
-#define MAJOR_VERSION        0
-#define MINOR_VERSION        9
-#define REVISION_NUM         3
+#define MAJOR_VERSION        1
+#define MINOR_VERSION        0
+#define REVISION_NUM         0
 
 //MAJOR_VERSION.MINOR_VERSION.REVISION_NUM
 
