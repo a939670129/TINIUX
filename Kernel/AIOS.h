585d0ac107b61acf6bbd7aaaf7e2a9c1d2d0b8f8
SenseRate <zhangxb@aliyun.com>
2017-03-21 23:18:22 +0800

    Update AIOS.h

--- Kernel/AIOS.h
+++ Kernel/AIOS.h
@@ -55,7 +55,7 @@ extern "C" {
 
 #define MAJOR_VERSION        1
 #define MINOR_VERSION        0
-#define REVISION_NUM         1
+#define REVISION_NUM         2
 
 //MAJOR_VERSION.MINOR_VERSION.REVISION_NUM
 
